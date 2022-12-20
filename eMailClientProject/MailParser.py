df0addcb9dea767657882cc23290bf86b9030819
Rashat Sarmanaev <110100353+Zysperro@users.noreply.github.com>
2022-12-20 08:40:47 +0500

    Add files via upload

--- eMailClientProject/MailParser.py
+++ eMailClientProject/MailParser.py
@@ -1,13 +1,136 @@
 import eml_parser
 import email
+import quopri
 import html2text as h2t
 from urlextract import URLExtract
-import re
+import imaplib
+import base64
 
 from os import walk
 
 path = './SpamFolder/'
 out_path = './OutputMsgs/'
+
+def Get_Messages_From_Mail(mail_address: str, mail_pass:str, server_domain:str):
+    """ Функция получения писем из email
+
+    Args: 
+        mail_address: Адрес почты
+        mail_pass: Пароль от почты
+        server_domain: адрес imap сервера (yandex.ru / google.com / mail.ru...)
+
+    Returns:
+        dict[]: Массив словарей отпарсенных частей сообщений
+            dict[['body'], ['headers']]:\n
+                dict['body']:  - тело\n
+                    dict['body']['text'] - текстовая часть из html письма\n
+                    dict['body']['urls'] - ссылки из текстовой составляющей html письма\n
+                dict['header']: - заголовки письма\n
+                    dict['header']['from'] - От кого (пример - no-reply.dns@email.dns-shop.ru)\n
+                    dict['header']['received_domain'][] - Домен(ы) отправителя ['ip1.email.dns-shop.ru']\n
+                    dict['header']['received_ip'][] - IP адрес(а) отправителя (цепочки) ['185.31.80.190']\n
+                    dict['header']['date'] - Дата отправки\n
+                    dict['header']['subject'] - Тема письма\n
+
+                    dict['header']:
+                        dict['header']['header']['message-id'] - Id письма, можно достать Uid ['40227=d46c7fba-9b87-4029-acf8-6e2e9329226d=-2=10338015@links.email.dns-shop.ru']\n
+    """
+
+    parsed_messages = []
+    URLParser = URLExtract()
+    ep = eml_parser.EmlParser()
+    host = f'imap.{server_domain}'
+    print(host)
+    imap = imaplib.IMAP4_SSL(host)
+    imap.login(mail_address, mail_pass)
+    imap.select("INBOX")
+    (inbox, data) = imap.search(None, 'ALL')
+    for num in data[0].split():
+        (typ, data) = imap.fetch(num, '(RFC822)')
+        for response_part in data:
+            if isinstance(response_part, tuple):
+                #f = email.message_from_bytes(response_part[1])
+                #file = f.as_bytes()
+                #print(file)
+                (msg, parsed_msg) = ep.decode_email_bytes(response_part[1])
+                body = "" # НАДО ДОБАВИТЬ ПРОВЕРКУ ENCODING
+                html_body = ""
+                if msg.is_multipart():
+                    body = msg
+                    #while isinstance(body, email.message.Message):
+                    #while body.is_multipart():
+                    for part in body.walk():
+                        if part.get_content_type() == 'text/html':
+                            html_body = part.get_payload()
+                            print(part['Content-Transfer-Encoding'])
+                            if 'base64' in part['Content-Transfer-Encoding']:
+                                html_body = base64.b64decode(html_body).decode()
+                            if 'quoted-printable' in part['Content-Transfer-Encoding']:
+                                try:
+                                    html_body = quopri.decodestring(html_body).decode('utf-8')
+                                except Exception as e:
+                                    print(f'Cant decode in UTF8 Q-P: {e}')
+                                try:
+                                    html_body = quopri.decodestring(html_body).decode('windows-1251')
+                                except Exception as e:
+                                    print(f'Cant decode in win-1251 Q-P: {e}')
+                                try:
+                                    html_body = quopri.decodestring(html_body).decode('koi8-r')
+                                except Exception as e:
+                                    print(f'Cant decode in win-1251 Q-P: {e}')    
+                            parsed_msg['header']['html'] = html_body
+                        if part.get_content_type() == 'text/plain':
+                            text_body = part.get_payload()
+                            print(type(text_body))
+                            print(part['Content-Transfer-Encoding'])
+                            if 'base64' in part['Content-Transfer-Encoding']:
+                                text_body = base64.b64decode(text_body).decode()
+                            if 'quoted-printable' in part['Content-Transfer-Encoding']:
+                                print(part['Content-Type'])
+                                try:
+                                    text_body = quopri.decodestring(text_body).decode('utf-8')
+                                except Exception as e:
+                                    print(f'Cant decode in UTF8 Q-P: {e}')
+                                try:
+                                    text_body = quopri.decodestring(text_body).decode('windows-1251')
+                                except Exception as e:
+                                    print(f'Cant decode in win-1251 Q-P: {e}')
+                                try:
+                                    text_body = quopri.decodestring(text_body).decode('koi8-r')
+                                except Exception as e:
+                                    print(f'Cant decode in win-1251 Q-P: {e}')
+                                print(type(text_body))
+                            parsed_msg['header']['text'] = text_body
+                            parsed_msg['header']['urls'] = set(URLParser.find_urls(text_body))
+                    #html_body = body
+                    #body = h2t.html2text(body)
+                else:
+                        html_body = msg.get_payload()
+                        if msg.get_content_type() == 'text/html' or 'text/plain':
+                            if 'base64' in msg['Content-Transfer-Encoding']:
+                                html_body = base64.b64decode(html_body).decode()
+                            if 'quoted-printable' in part['Content-Transfer-Encoding']:
+                                try:
+                                    html_body = quopri.decodestring(html_body).decode('utf-8')
+                                except Exception as e:
+                                    print(f'Cant decode in UTF8 Q-P: {e}')
+                                try:
+                                    html_body = quopri.decodestring(html_body).decode('windows-1251')
+                                except Exception as e:
+                                    print(f'Cant decode in win-1251 Q-P: {e}')
+                                try:
+                                    html_body = quopri.decodestring(html_body).decode('koi8-r')
+                                except Exception as e:
+                                    print(f'Cant decode in win-1251 Q-P: {e}')
+                        else:
+                            html_body = "Неподдерживаемый формат"
+                        parsed_msg['header']['html'] = html_body
+                        parsed_msg['header']['text'] = html_body
+                        parsed_msg['header']['urls'] = set(URLParser.find_urls(html_body))
+                print('Adding msg')
+                parsed_messages.append(parsed_msg)
+    return parsed_messages
+
 
 def Get_Parsed_EML_Messages(eml_files: bytes):
     """ Функция для парсинга EML файлов (email писем)
@@ -37,25 +160,137 @@ def Get_Parsed_EML_Messages(eml_files: bytes):
     ep = eml_parser.EmlParser()
 
     for file in eml_files:
+        if file is None:
+            continue
         # Парсим файлы, создаем словари, заполняем массив
-        (msg, parsed_msg) = ep.decode_email_bytes(file)
-        body = "" # НАДО ДОБАВИТЬ ПРОВЕРКУ ENCODING
-        if msg.is_multipart():
-            #body = h2t.html2text(msg.get_payload()[-1].get_payload()[-1])
-            body = msg.get_payload()[-1].get_payload()[-1]
-            while not isinstance(body, str):
-                body = body.get_payload()[-1]
-            body = h2t.html2text(body)
-        else:
-            try:
-                body = h2t.html2text(msg.get_payload(decode=True).decode(encoding='cp1252'))
-            except:
-                body = "None"
+        # (msg, parsed_msg) = ep.decode_email_bytes(file)
+        # body = ""
+        # html_body = ""
+        # try:
+        #     #if not msg is None:
+        #     #    body = msg.get_payload(decode=True).decode()
+        #     #else:
+        #     msg = email.message_from_bytes(file)
+        #     for part in msg.walk():
+        #         if part.get_content_type() == 'text/html':
+        #             body = part.get_payload()
+        #     #print(body)
+        #     #parsed_msg['body'] = body
+        #     print("Got msg from file.")
+        # except Exception as e:
+        #     print(f"Couldn't get msg from file: {e} : {email.message_from_bytes(file)['subject']}")
+        # parsed_msg['header']['urls'] = set(URLParser.find_urls(body))
+        # parsed_msg['body'] = body
 
-        parsed_msg['header']['urls'] = set(URLParser.find_urls(body))
-        parsed_msg['header']['text'] = body
-        
+        #===========================
+        (msg, parsed_msg) = ep.decode_email_bytes(file)
+        body = "" # email.message.Message
+        text_body = ""
+        html_body = ""
+        msg = email.message_from_bytes(file)
+
+        if msg.is_multipart():
+            body = msg
+            #while isinstance(body, email.message.Message):
+            #while body.is_multipart():
+            for part in body.walk():
+                if part.get_content_type() == 'text/html':
+                    html_body = part.get_payload()
+                    print(part['Content-Transfer-Encoding'])
+                    if 'base64' in part['Content-Transfer-Encoding']:
+                        html_body = base64.b64decode(html_body).decode()
+                    if 'quoted-printable' in part['Content-Transfer-Encoding']:
+                        try:
+                            html_body = quopri.decodestring(html_body).decode('utf-8')
+                        except Exception as e:
+                            print(f'Cant decode in UTF8 Q-P: {e}')
+                        try:
+                            html_body = quopri.decodestring(html_body).decode('windows-1251')
+                        except Exception as e:
+                            print(f'Cant decode in win-1251 Q-P: {e}')
+                        try:
+                            html_body = quopri.decodestring(html_body).decode('koi8-r')
+                        except Exception as e:
+                            print(f'Cant decode in win-1251 Q-P: {e}')    
+                    parsed_msg['header']['html'] = html_body
+                if part.get_content_type() == 'text/plain':
+                    text_body = part.get_payload()
+                    print(type(text_body))
+                    print(part['Content-Transfer-Encoding'])
+                    if 'base64' in part['Content-Transfer-Encoding']:
+                        text_body = base64.b64decode(text_body).decode()
+                    if 'quoted-printable' in part['Content-Transfer-Encoding']:
+                        print(part['Content-Type'])
+                        try:
+                            text_body = quopri.decodestring(text_body).decode('utf-8')
+                        except Exception as e:
+                            print(f'Cant decode in UTF8 Q-P: {e}')
+                        try:
+                            text_body = quopri.decodestring(text_body).decode('windows-1251')
+                        except Exception as e:
+                            print(f'Cant decode in win-1251 Q-P: {e}')
+                        try:
+                            text_body = quopri.decodestring(text_body).decode('koi8-r')
+                        except Exception as e:
+                            print(f'Cant decode in win-1251 Q-P: {e}')
+                        print(type(text_body))
+                    parsed_msg['header']['text'] = text_body
+                    parsed_msg['header']['urls'] = set(URLParser.find_urls(text_body))
+            #html_body = body
+            #body = h2t.html2text(body)
+        else:
+                html_body = msg.get_payload()
+                if msg.get_content_type() == 'text/html' or 'text/plain':
+                    if 'base64' in msg['Content-Transfer-Encoding']:
+                        html_body = base64.b64decode(html_body).decode()
+                    if 'quoted-printable' in part['Content-Transfer-Encoding']:
+                        try:
+                            html_body = quopri.decodestring(html_body).decode('utf-8')
+                        except Exception as e:
+                            print(f'Cant decode in UTF8 Q-P: {e}')
+                        try:
+                            html_body = quopri.decodestring(html_body).decode('windows-1251')
+                        except Exception as e:
+                            print(f'Cant decode in win-1251 Q-P: {e}')
+                        try:
+                            html_body = quopri.decodestring(html_body).decode('koi8-r')
+                        except Exception as e:
+                            print(f'Cant decode in win-1251 Q-P: {e}')
+                else:
+                    html_body = "Неподдерживаемый формат"
+                parsed_msg['header']['html'] = html_body
+                parsed_msg['header']['text'] = html_body
+                parsed_msg['header']['urls'] = set(URLParser.find_urls(html_body))
+
+        last_msg = body
+        # for i in range(1):
+        #     part = last_msg
+        #     #print(part['Content-Type'])
+        #     if 'text/html' in part['Content-Type']:
+        #         html_body = part.get_payload()
+        #         print(part['Content-Transfer-Encoding'])
+        #         if 'base64' in part['Content-Transfer-Encoding']:
+        #             html_body = base64.b64decode(html_body).decode()
+        #         if 'quoted-printable' in part['Content-Transfer-Encoding']:
+        #             html_body = quopri.decodestring(html_body)
+        #         parsed_msg['header']['html'] = html_body
+        #     if 'text/plain' in part['Content-Type']:
+        #         text_body = part.get_payload()
+        #         print(part['Content-Transfer-Encoding'])
+        #         if 'base64' in part['Content-Transfer-Encoding']:
+        #             text_body = base64.b64decode(text_body).decode()
+        #         if 'quoted-printable' in part['Content-Transfer-Encoding']:
+        #             text_body = quopri.decodestring(text_body)   
+
+        # parsed_msg['header']['urls'] = set(URLParser.find_urls(text_body))
+        # parsed_msg['header']['text'] = text_body
+        # parsed_msg['header']['html'] = html_body
+
         parsed_messages.append(parsed_msg)
+
+        #===========================
+
+        #parsed_messages.append(parsed_msg)
     return parsed_messages
     
 
--- eMailClientProject/app.py
+++ eMailClientProject/app.py
@@ -1,18 +1,124 @@
 import MailParser as mp
 import flask
+import os
+import shutil
 from os import walk
+import werkzeug
 
 app = flask.Flask('MailApp', template_folder='./venv/templates')
 app.config['EXPLAIN_TEMPLATE_LOADING'] = True
+app.secret_key = os.urandom(24)
 
 eml_files = []
-eml_files_path = "C:/Users/user/source/repos/SpamFilterProject/SpamFolder/"
+eml_files_path = './venv/UploadFiles/' #"C:/Users/user/source/repos/SpamFilterProject/SpamFolder/"
+ALLOWED_EXTENSIONS = {'eml'}
+mail_address = "Rashat03@yandex.ru"
+mail_pass = ""
+mail_domain = "yandex.ru"
+selectedMessage = []
+selectedMessageText = ""
+selectedMessageSubject = "Тема выбранного письма"
+messages = list()
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def show_index_page():
-    eml_files = get_eml_files(eml_files_path)
-    messages = mp.Get_Parsed_EML_Messages(eml_files)
-    return flask.render_template('index.html', inbox = messages)
+    global selectedMessageText
+    global selectedMessageSubject
+    global messages
+    messages.clear()
+
+    #if flask.request.method == 'POST':
+    #    selectmsg()
+    #eml_files = get_eml_files(eml_files_path)
+    #messages = get_email()
+    #messages = mp.Get_Parsed_EML_Messages(eml_files)
+    #try:
+    #    selectedMessageText = messages[1]['body']
+    #except Exception as e:
+    #    print(e)
+    return flask.render_template('index.html', selectedMessageF = selectedMessage, inbox = messages, selectedMessageHTML = selectedMessageText, selectedMessageSubjectF = selectedMessageSubject)
+
+# Выбор письма и открытие его
+@app.route('/selectmsg', methods=['POST'])
+def selectmsg():
+    global selectedMessage
+    global selectedMessageText
+    global selectedMessageSubject
+    global messages
+    selectedMessageSubject = flask.request.form['messsageListItem']
+    try:
+        for msg in messages:
+            if(msg['header']['subject'] == selectedMessageSubject):
+                selectedMessage.clear()
+                selectedMessage.append(msg)
+                selectedMessageText = msg['header']['html']
+    except Exception as e:
+        print(f'Couldn\'t load HTML msg: {e}')
+    return flask.render_template('index.html', inbox = messages, selectedMessageF = selectedMessage, selectedMessageHTML = selectedMessageText, selectedMessageSubjectF = selectedMessageSubject)
+
+def get_email():
+    return mp.Get_Messages_From_Mail(mail_address, mail_pass, mail_domain)
+
+def clean_folder():
+    for filename in os.listdir(eml_files_path):
+        file_path = os.path.join(eml_files_path, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
+    return
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/get_files', methods=['POST', 'GET'])
+def get_files():
+    clean_folder()
+    global eml_files
+    global messages
+    if flask.request.method == 'GET':
+        return flask.render_template('index.html', inbox = messages, selectedMessageHTML = selectedMessageText, selectedMessageSubjectF = selectedMessageSubject)
+
+    if flask.request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in flask.request.files:
+            print('No file part')
+            flask.flash('No file part')
+            return flask.redirect(flask.request.url)
+        upload_files = flask.request.files.getlist("file")
+        for file in upload_files:
+            if file.filename == '':
+                print('No selected file')
+                flask.flash('No selected file')
+                return flask.redirect(flask.request.url)
+            if file and allowed_file(file.filename):
+                filename = werkzeug.utils.secure_filename(file.filename)
+                file.save(os.path.join(eml_files_path, filename))
+                print(file.filename)
+        eml_files = get_eml_files(eml_files_path)
+        messages = mp.Get_Parsed_EML_Messages(eml_files)
+    return flask.render_template('index.html', inbox = messages, selectedMessageHTML = selectedMessageText, selectedMessageSubjectF = selectedMessageSubject)
+
+@app.route('/get_mail', methods=['POST'])
+def get_mail():
+    # Загрузка писем из почты
+    global messages
+    global mail_domain
+    global mail_address
+    global mail_pass
+    print(flask.request.form['userpw'])
+    print(flask.request.form['username'])
+    print(flask.request.form['providerList'])
+    mail_domain = flask.request.form['providerList']
+    mail_address = flask.request.form['username']
+    mail_pass = flask.request.form['userpw']
+    messages = get_email()
+    return flask.render_template('index.html', inbox = messages, selectedMessageHTML = selectedMessageText, selectedMessageSubjectF = selectedMessageSubject)
+
 
 def get_eml_files(folder_path):
     eml_files_list = []
@@ -24,11 +130,11 @@ def get_eml_files(folder_path):
     for file in eml_paths_list:
         if(file.endswith('meta')):
             continue
-        print(file)
+        #print(file)
         with open(folder_path+file, 'rb') as f:
             eml_files_list.append(f.read())
 
     return eml_files_list
 
-if __name__ == '__main__':
+if __name__ == '__main__':  
     app.run('0.0.0.0', port=9979, debug=True)
